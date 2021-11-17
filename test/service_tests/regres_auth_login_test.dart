b2b75ce51e959ba9b3931184acc547ffd0e0734c
abdulla-bey01 <abdullabey085@gmail.com>
2021-11-18 00:53:07 +0400

    auth_net was moved to base folder

--- test/service_tests/regres_auth_login_test.dart
+++ test/service_tests/regres_auth_login_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mvvm_simple/app/auth/concrency/reqres/reqres_auth_network_manager.dart';
-import 'package:mvvm_simple/app/auth/abstraction/i_auth_network_manager.dart';
+import 'package:mvvm_simple/app/auth/abstraction/base/i_auth_network_manager.dart';
 import 'package:mvvm_simple/data/dtos/login_dto.dart';
 import 'package:uuid/uuid.dart';
 
